6e36756b422119ae32e40c07b18b64ca7950099a
shilpisharma13 <shilpi13@gmail.com>
2022-07-28 21:25:55 +0930

    challenge created

---
+++ resident-program-app/src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import Setup from './pages/Setup'
+
+function App() {
+  return <Setup />
+}
+
+export default App
---
+++ resident-program-app/src/pages/Setup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Setup = () => {
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  )
+}
+
+export default Setup
